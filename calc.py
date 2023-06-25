4b5d4dd65a890bd41928ff3b08c6a6166ce6db3d
granthsxena <137663384+granthsxena@users.noreply.github.com>
2023-06-25 11:49:51 +1000

    Update calc.py
    adding more shapes

--- calc.py
+++ calc.py
@@ -17,9 +17,15 @@ def on_shape_selected(event):
         radius_label.pack()
         dimension_entry = ttk.Entry(dimensions_frame)
         dimension_entry.pack()
-    elif shape == "Square":
-        # For square, ask for side length
+    elif shape == "Square" or shape == "Hexagon":
+        # For square and hexagon, ask for side length
         side_label = ttk.Label(dimensions_frame, text="Side length:")
+        side_label.pack()
+        dimension_entry = ttk.Entry(dimensions_frame)
+        dimension_entry.pack()
+    elif shape == "Triangle":
+        # For triangle, ask for side length
+        side_label = ttk.Label(dimensions_frame, text="Side length (equilateral):")
         side_label.pack()
         dimension_entry = ttk.Entry(dimensions_frame)
         dimension_entry.pack()
@@ -46,20 +52,30 @@ def on_calculate_button_click():
             result = dimension ** 2
         else:
             result = 4 * dimension
+    elif shape == "Hexagon":
+        if calculation == "Area":
+            result = (3 * math.sqrt(3) * dimension ** 2) / 2
+        else:
+            result = 6 * dimension
+    elif shape == "Triangle":
+        if calculation == "Area":
+            result = (math.sqrt(3) / 4) * dimension ** 2
+        else:
+            result = 3 * dimension
 
-    messagebox.showinfo("Result", f"The {calculation} of the {shape} is {result}")
+    messagebox.showinfo("Result", f"The {calculation} of the {shape} is {result:.2f}")
 
 
 # Create the main window
 root = tk.Tk()
-root.title("Shape Calculator")
+root.title("GeoCalc")
 
 # Create a label for shape selection
 shape_label = ttk.Label(root, text="Select a shape:")
 shape_label.pack()
 
 # Create a combobox for shape selection
-shape_combobox = ttk.Combobox(root, values=["Circle", "Square"])  # Add more shapes if needed
+shape_combobox = ttk.Combobox(root, values=["Circle", "Square", "Hexagon", "Triangle"])
 shape_combobox.bind("<<ComboboxSelected>>", on_shape_selected)
 shape_combobox.pack()
 
@@ -72,7 +88,7 @@ calculation_label = ttk.Label(root, text="Select a calculation:")
 calculation_label.pack()
 
 # Create a combobox for calculation selection
-calculation_combobox = ttk.Combobox(root, values=["Area", "Perimeter"])  # Add more calculations if needed
+calculation_combobox = ttk.Combobox(root, values=["Area", "Perimeter"])
 calculation_combobox.pack()
 
 # Create a calculate button
